5239235542f518ccc01fc458c6ecc8681a55e313
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-09-17 21:03:21 -0500

    Movie Widget API

---
+++ client/components/directives/mb-movie/mb-movie.js
@@ -0,0 +1,30 @@
+(function(){
+  'use strict';
+
+  angular.module('mbMovieModule', [])
+  .factory('MovieApi', ['$http', function($http){
+    function info(title){
+      return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies.json?q='+ title  +'&page_limit=1&page=1&apikey=3sjsan7wm92dace3hedn94r2&callback=JSON_CALLBACK');
+    }
+    return {info:info};
+  }])
+  .directive('mbMovie', [function(){
+    var o = {};
+
+    o.restrict    = 'A';
+    o.templateUrl = '/components/directives/mb-movie/mb-movie.html';
+    o.scope       = {title:'@'};
+    o.link        = function(scope, element, attrs){
+                      };
+
+    o.controller  = ['$scope', 'MovieApi', function($scope, MovieApi){
+                        MovieApi.info($scope.title).then(function(response){
+                          $scope.movie = response.data.movies[0];
+                          $scope.poster = $scope.movie.posters.thumbnail.replace(/_tmb/, '_pos');
+                      });
+                    }];
+
+    return o;
+  }]);
+})();
+
